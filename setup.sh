ed6bff3ad3ddf5b1e97f5dc4868e64ed72702f70
Peter Sutton <petersutton2009@gmail.com>
2011-11-18 17:44:33 +0000

    Updated setup.sh to install publicly in the person's home directly.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ set -o errexit
 set -o nounset
 set -o verbose
 
-# Must NOT contain argument seperator. Glob supported.
 readonly THREADS="$((
     ($(sed -n 's/^processor\s*:\s*\([0-9]\+\)$/\1/p' /proc/cpuinfo \
         | tail -n 1) \
@@ -12,13 +11,14 @@ readonly THREADS="$((
 
 # == Python 3.2 == #
 readonly PY_ARCHIVE='python.tar.xz'
+# Must NOT contain argument seperator. Glob supported.
 readonly PY_DIR='Python-*'
 readonly PY_URL='http://python.org/ftp/python/3.2.2/Python-3.2.2.tar.xz'
 
 cd /tmp
-wget -O "${ARCHIVE}" "${URL}"
-xz --decompress --stdout "${ARCHIVE}" | tar -x
-cd ${DIR}
+wget -O "${PY_ARCHIVE}" "${PY_URL}"
+xz --decompress --stdout "${PY_ARCHIVE}" | tar -x
+cd ${PY_DIR}
 ./configure --prefix="${HOME}" --exec-prefix="${HOME}"
 make -j "${THREADS}" install
 
@@ -34,8 +34,16 @@ find -name '*.py' -exec sed -i 's/print\s*\([^(]*\)$/print(\1)/g' {} \;
 python3.2 setup.py install
 
 # == Emailit == #
-readonly EI_URL='git//github.com/Man-UP/emailit.git'
+readonly EI_URL='git://github.com/Man-UP/emailit.git'
 
-cd "${HOME}"
+cd "${HOME}/public"
 git clone "${EI_URL}"
+
+# == Permissions == #
+cd
+readonly PY_INSTALL_DIR='bin/ include/ lib/ share/ public/'
+find ${PY_INSTALL_DIR} -type d -exec chmod 755 {} \;
+find ${PY_INSTALL_DIR} -type f -perm -u=r -exec chmod go+r {} \;
+find ${PY_INSTALL_DIR} -type f -perm -u=x -exec chmod go+x {} \;
+
 
